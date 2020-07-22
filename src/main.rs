123e20d1ab7834c7776d0814c58d648e27491494
vspecky <thelonenerd9913@gmail.com>
2020-07-22 20:59:00 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate ggez;
+mod game;
+
+use ggez::{
+    event::{self, EventHandler},
+    graphics, Context, ContextBuilder, GameResult,
+};
+
+const WIN_WIDTH: u32 = 800;
+const WIN_HEIGHT: u32 = 800;
+
+fn main() {
+    /*
+    let (mut ctx, mut event_loop) =
+        ContextBuilder::new("Rust Chess", "vSpecky")
+            .build()
+            .unwrap();
+    */
+}
