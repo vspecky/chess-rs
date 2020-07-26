650ada3fa4677ed7a2c0e210d198ce8323e8d134
vspecky <thelonenerd9913@gmail.com>
2020-07-26 18:49:55 +0530

    Added window title

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate ggez;
 mod game;
 
 use ggez::{
-    conf::WindowMode,
+    conf::{WindowMode, WindowSetup},
     event::{self, EventHandler},
     graphics, Context, ContextBuilder, GameResult,
 };
@@ -15,11 +15,14 @@ const WIN_HEIGHT: u32 = 800;
 fn main() -> GameResult {
     let win_mode = WindowMode::default().dimensions(800., 800.);
 
+    let win_setup = WindowSetup::default().title("Chess.rs");
+
     let mut asset_path = path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
     asset_path.push("src");
     asset_path.push("assets");
 
     let (mut ctx, mut event_loop) = ContextBuilder::new("Rust Chess", "vSpecky")
+        .window_setup(win_setup)
         .window_mode(win_mode)
         .add_resource_path(asset_path)
         .build()
